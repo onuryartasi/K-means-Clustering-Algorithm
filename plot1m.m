20aadd5fb66b64d6be8b252c0fc1bd98b1722f82
Onur Yartaşı <onuryartasi@live.com>
2017-04-27 23:39:18 +0400

    Multi Clustering

--- plot1m.m
+++ plot1m.m
@@ -1,6 +1,6 @@
 n=200;
 X=round(1+127*rand(2,n));
-cluster=3;
+cluster=7;
 C=round(1+127*rand(2,cluster));
 colorspec = {'k','b','r','g','y',[.5 .6 .7],[.8 .2 .6]}; %kümelere renk atama
 Dist=zeros(cluster,n);
@@ -8,14 +8,16 @@ Clusters=zeros(cluster,n);
 oldC=zeros(2,cluster);
 Centroid=zeros(1,cluster);
 
-%uzaklýk hesaplama
+
+ 
+%Distance calculation
 for i=1:n
     for j=1:cluster
         Dist(j,i)=sqrt((C(1,j)-X(1,i)).^2+(C(2,j)-X(2,i)).^2);
     end
 end
 
-%Uzaklýklara göre Kümeleme iþlemi ve eleman sayýlarý
+%Clustering and set element counts by distance
 for i=1:n
     min=0;
    for j=1:cluster
@@ -37,26 +39,26 @@ for i=1:n
     
 end
 
-%Centroidlerin yedeklenmesi
+%Back-up of centroids
 for j=1:cluster
   oldC(1,j)=C(1,j);
   oldC(2,j)=C(2,j);
     
 end
-%Orta noktalarýn yazdýrýlmasý
+%Printing Centroid
 
 hold on
 h1=plot(C(1:cluster-1:2*cluster),C(2:cluster-1:2*cluster),'yx');
 
 C=zeros(2,cluster);
 
-%Yeni Centroid hesap
+%New centroid calculation
 for j=1:cluster
     for i=1:n
        if(Clusters(j,i)==1)
            C(1,j)=C(1,j)+X(1,i)/Centroid(j);
            C(2,j)=C(2,j)+X(2,i)/Centroid(j);
-       elseif (Centroid(j)==0) %Kümede hiçbir eleman yoksa eski centroid
+       elseif (Centroid(j)==0) %If there are no elements in the cluster, the old centroid
            C(1,j)=oldC(1,j);
            C(2,j)=oldC(2,j);
        end
@@ -67,6 +69,9 @@ end
 
 hold on
 set(h1,'Visible','off')
-h2 = plot(C(1:cluster-1:2*cluster),C(2:cluster-1:2*cluster),'gx');
+h2 = plot(C(1:2:2*cluster),C(2:2:2*cluster),'ko');
+
+
+disp(C)
 
 
